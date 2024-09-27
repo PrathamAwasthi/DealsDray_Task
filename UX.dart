1df7468e5ef856ba5008963fd88d0db1b228ad69
Pratham Awasthi <7068prathamawasthi@gmail.com>
2024-09-27 09:05:49 +0530

    Add files via upload

---
+++ UX.dart
@@ -0,0 +1,8 @@
+
+class Account {
+  String? email;
+  String? password;
+  String? phoneNo;
+  String? refCode;
+  Account({required this.email, required this.password, required this.phoneNo, this.refCode});
+}
